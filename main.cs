9f2df99ba8a8c39693210297352f982a85af31f2
Pedro Andrade <49775242+Pedroandr@users.noreply.github.com>
2024-02-24 18:17:40 +0000

    Add a replace function.

--- main.cs
+++ main.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 
 class Program {
+  public static int turn = 0;
   static List<string> places = new List<string>{"1","2","3","4","5","6","7","8","9"};
   
   public static string PrintBoard(){
@@ -9,8 +10,23 @@ class Program {
     string board = ($"{places[0]}|{places[1]}|{places[2]} \n-----\n{places[3]}|{places[4]}|{places[5]} \n-----\n{places[6]}|{places[7]}|{places[8]}");
     return board;
     }
+  public static String playerSymbol(){
+    if (turn == 0){
+      turn++;
+      return "X";
+    }
+    else{
+      turn--;
+      return "0";
+      }
+  }
+  public static void Replace(string place){
+    places.Remove(place);
+    places.Insert(int.Parse(place)-1, playerSymbol());
+  }
   
   public static void Main (string[] args) {
     Console.WriteLine(PrintBoard());
+    
   }
 }
